9824e3bd8aa33e661b900878b137958597f0232c
Aaron Levine <aclevine@brandeis.edu>
2014-10-14 21:23:52 -0400

    classifier step

--- src/classify/ClassifyMapred.java
+++ src/classify/ClassifyMapred.java
@@ -6,41 +6,89 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.TreeMap;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-import train.GetCountMapred;
-import train.GetCountMapred.GetCountMapper;
-import train.GetCountMapred.GetCountReducer;
+import utils.StringDoubleList;
+import utils.StringDoubleList.StringDouble;
 import utils.StringIntegerList;
+import utils.StringIntegerList.StringInteger;
+
+import java.lang.*;
 
 public class ClassifyMapred {
 
-	public static class ClassifyMapper extends
-		Mapper<Text, Text, Text, Text> {
+	public static class ClassifyMapper extends Mapper<Text, Text, Text, Text> {
 	
+		public static HashMap<String, HashMap> labelScores = new HashMap<String, HashMap>();
+		public static Double threshold = 0.5;
+		
 		protected void setup(Mapper<Text, Text, Text, Text>.Context context)
 				throws IOException, InterruptedException {
-			// TODO: read profession probabilities, feature given profession proabilties
+			// TODO: read profession probabilities, feature given profession probabilites
 
+			InputStream is = this.getClass().getResourceAsStream("prob.txt"); // check with Rachel how to load from hdfs again?
+			BufferedReader br = new BufferedReader(new InputStreamReader(is)); //Open text				
 			
+			String line;
+			while ((line = br.readLine()) != null){
+            	String[] labelIndices = line.split("\\s+", 1);
+            	StringDoubleList list = new StringDoubleList();	
+    			list.readFromString(labelIndices[0].toString());
+    			
+    			HashMap<String, Double> temp = new HashMap<String, Double>();
+    			for (StringDouble index : list.getIndices()) {    			
+    				String lemma = index.getString();
+    				Double prob = index.getValue();
+    				temp.put(lemma, prob);
+    			}
+    			labelScores.put(labelIndices[0], temp);
+            }
 			super.setup(context);
-					
 		}
 		
 		public void map(Text articleId, Text indices, Context context)
 				throws IOException, InterruptedException {	
 			// TODO: build: 
-			// < Title,  <top 3 predictions above threshold> ] > 		
+			// < Title,  <top 3 predictions above threshold> ] > 
+			StringIntegerList list = new StringIntegerList();	
+			list.readFromString(indices.toString());
+	        TreeMap<Double,String> guesses = new TreeMap<Double,String>(Collections.reverseOrder());			
+
+	        for (String label : labelScores.keySet()) {
+	        	HashMap<String, Double> scores = labelScores.get(label);
+	        	Double prob = Math.log( scores.get("__PROFESSION__") ); // need to get Tifara to make a name for this
+	        	for (StringInteger index : list.getIndices()) {	
+					String lemma = index.getString();
+					int freq = index.getValue();
+					if (scores.containsKey(lemma)) {
+						prob += freq * Math.log(scores.get(lemma));
+					}	
+	        	}	        	
+	        	guesses.put(prob, label);
+	        }
+	         
+			String top3 = "";	
+	        for (Double prob: guesses.keySet()){
+	        	if (prob >= threshold) {
+	        		top3 += ("," +  guesses.get(prob));
+	        	}
+	        	else {
+	        		context.write(articleId, new Text(":" + top3));
+	        		break;
+	        	}
+	        }
 		}
 	}
 
@@ -70,5 +118,6 @@ public class ClassifyMapred {
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 		job.getConfiguration().set("mapreduce.job.queuename", "hadoop14");
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
 	}		
 }
---
+++ src/classify/GetTestMapred.java
@@ -0,0 +1,96 @@
+package classify;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.io.LongWritable;
+
+import utils.StringIntegerList;
+import utils.StringIntegerList.StringInteger;
+import edu.umd.cloud9.collection.wikipedia.WikipediaPage;
+
+/**
+ * This class is used for Section A of assignment 1. You are supposed to
+ * implement a main method that has first argument to be the dump wikipedia
+ * input filename , and second argument being an output filename that only
+ * contains articles of people as mentioned in the people auxiliary file.
+ */
+public class GetTestMapred {
+
+	//@formatter:off
+	/**
+	 * Input:
+	 * 		articleTitle 	lemmaCounts
+	 * Output
+	 * 		articleTitle 	lemmaCounts
+	 * @author Aaron
+	 *
+	 */
+	//@formatter:on
+	public static class GetTestMapper extends Mapper<Text, Text, Text, Text> {
+		
+		public static Set<String> peopleArticlesTitles = new HashSet<String>();
+
+		@Override
+		protected void setup(Mapper<Text, Text, Text, Text>.Context context)
+				throws IOException, InterruptedException {
+			//read file
+			InputStream is = this.getClass().getResourceAsStream("profession_test.txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(is)); //Open text				
+			String line;
+            while ((line = br.readLine()) != null) {
+                peopleArticlesTitles.add(line);
+            }
+			super.setup(context);
+		}
+
+		@Override
+		public void map(Text articleId, Text indices, Context context)
+				throws IOException, InterruptedException {
+			String title = articleId.toString().replaceAll("[\\r\\n]+", " ");
+			if(peopleArticlesTitles.contains(title)) {
+				context.write(new Text(title), new Text(indices.toString()));
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		//Get Articles
+		Configuration conf = new Configuration();        
+	    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+	    if (otherArgs.length != 2) {
+	      System.err.println("Usage: GetArticlesMapred <in> <out>");
+	      System.exit(2);
+	    }		
+		Job job = Job.getInstance(conf, "filter down to test data");
+        job.setJarByClass(GetTestMapred.class);
+        job.setMapperClass(GetTestMapper.class);
+        job.setNumReduceTasks(0);
+		
+        job.setInputFormatClass(KeyValueTextInputFormat.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(StringInteger.class);        
+
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+		job.getConfiguration().set("mapreduce.job.queuename", "hadoop14");
+        System.exit(job.waitForCompletion(true) ? 0 : 1);	
+	}
+}
